5703047d3cf05bc9ed0ed0b1dcbdc72602a6782c
Timazlo <timazloy04444@gmail.com>
2023-07-01 16:50:55 +0300

    upd: setup context for Search

--- src/App.js
+++ src/App.js
@@ -8,20 +8,24 @@ import Home from './pages/Home';
 import Header from './components/Header';
 import NotFound from './pages/NotFound';
 
+export const SearchContext = React.createContext();
+
 function App() {
    const [searchValue, setSearchValue] = React.useState('');
 
    return (
       <div className='wrapper'>
-         <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-         <div className='content'>
-            <div className='container'>
-               <Routes>
-                  <Route path='/' element={<Home searchValue={searchValue} />} />
-                  <Route path='*' element={<NotFound />} />
-               </Routes>
+         <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+            <Header />
+            <div className='content'>
+               <div className='container'>
+                  <Routes>
+                     <Route path='/' element={<Home searchValue={searchValue} />} />
+                     <Route path='*' element={<NotFound />} />
+                  </Routes>
+               </div>
             </div>
-         </div>
+         </SearchContext.Provider>
       </div>
    );
 }
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import styles from './Search.module.scss';
+import { SearchContext } from '../../App';
 
-function Search({ searchValue, setSearchValue }) {
+function Search() {
+   const { searchValue, setSearchValue } = React.useContext(SearchContext);
+
    return (
       <input
          className={styles.input}
