1c4b7cf24d84a84ccabe81d37e521fa86e65970b
Timazlo <timazloy04444@gmail.com>
2023-07-03 23:11:26 +0300

    upd: loadash.debounce

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,21 +2,43 @@ import React from 'react';
 import styles from './Search.module.scss';
 import close from '../../assets/img/close.svg';
 import { SearchContext } from '../../App';
+import debounce from 'lodash.debounce';
 
 function Search() {
+   const [value, setValue] = React.useState();
    const { searchValue, setSearchValue } = React.useContext(SearchContext);
+   const inputRef = React.useRef();
+
+   const updateSearchValue = React.useCallback(
+      debounce((str) => {
+         setSearchValue(str);
+      }, 700),
+      []
+   );
+
+   const onChangeInput = (e) => {
+      setValue(e);
+      updateSearchValue(e);
+   };
+
+   const clearSearchField = () => {
+      setSearchValue('');
+      setValue('');
+      inputRef.current.focus();
+   };
 
    return (
       <div className={styles.wrapper}>
          <input
+            ref={inputRef}
             className={searchValue ? `${styles.input} ${styles.active}` : styles.input}
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
+            value={value}
+            onChange={(e) => onChangeInput(e.target.value)}
             type='text'
             placeholder='Найти пиццу...'
          />
          {searchValue && (
-            <button onClick={() => setSearchValue('')} className={styles.close} type='button'>
+            <button onClick={() => clearSearchField()} className={styles.close} type='button'>
                <img src={close} alt='close' />
             </button>
          )}
