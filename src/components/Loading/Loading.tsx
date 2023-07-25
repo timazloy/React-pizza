995343d87b58ada5aa5f8aeebf62510ead28d99f
Timazlo <timazloy04444@gmail.com>
2023-07-25 21:03:59 +0300

    add: loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ThreeDots } from 'react-loader-spinner';
+import styles from './Loading.module.scss';
+
+const Loading: React.FC = () => {
+   return (
+      <div className={styles.loading}>
+         <ThreeDots height='80' width='80' radius='9' color='#fe5f1e' ariaLabel='loading' />
+      </div>
+   );
+};
+
+export default Loading;
