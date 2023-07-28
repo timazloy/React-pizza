1fbe3ed17a980b94ebe3a7aba947b6dcf6f1149d
Timazlo <timazloy04444@gmail.com>
2023-07-29 00:42:50 +0300

    upd: slider for create pizza double

--- src/pages/DoublePizzaCreate/DoublePizzaCreate.jsx
+++ src/pages/DoublePizzaCreate/DoublePizzaCreate.jsx
@@ -5,6 +5,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import axios from 'axios';
 import styles from './DoublePizzaCreate.module.scss';
+import arrowImg from '../../assets/img/back.svg';
 
 const DoublePizzaCreate: React.FC = () => {
    const [pizzas, setPizzas] = React.useState([]);
@@ -17,20 +18,40 @@ const DoublePizzaCreate: React.FC = () => {
       fetchData();
    }, []);
 
+   const CustomPrevArrow = (props) => {
+      const { onClick } = props;
+      return (
+         <button className={styles.arrow_prev} onClick={onClick}>
+            <img src={arrowImg} alt='arrow' />
+         </button>
+      );
+   };
+
+   const CustomNextArrow = (props) => {
+      const { onClick } = props;
+      return (
+         <button onClick={onClick}>
+            <img src={arrowImg} alt='arrow' />
+         </button>
+      );
+   };
+
    const settings = {
       infinite: true,
-      slidesToShow: 1,
-      // variableWidth: true,
+      slidesToShow: 1.6,
       slidesToScroll: 1,
+      swipe: false,
       vertical: true,
       verticalSwiping: true,
-      swipeToSlide: true,
+      // swipeToSlide: true,
       beforeChange: function (currentSlide, nextSlide) {
          console.log('before change', currentSlide, nextSlide);
       },
       afterChange: function (currentSlide) {
          console.log('after change', currentSlide);
-      }
+      },
+      prevArrow: <CustomPrevArrow />,
+      nextArrow: <CustomNextArrow />
    };
 
    return (
